b0df05eb27fdfa84f8707ce5ed67003eb561dc6a
WOORI-KWON <109576556+WOORI-KWON@users.noreply.github.com>
2022-08-04 17:07:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-view = [300, 200, 100, 500, 600]
+view = [300, 200, 100, 500, 600, 700]
 show_raw  = st.checkbox('show raw date')
 if show_raw == True:
     st.write('# raw data')
